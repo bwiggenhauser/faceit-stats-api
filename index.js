77b41ebd1c7b0fd1892c329e28cf1cc89c1bdfa9
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2022-04-02 17:14:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-const express = require("express")
-const app = express()
-const http = require("http")
-const server = http.createServer(app)
-const cors = require('cors');
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const cors = require("cors");
 
-app.use(express.static("public"))
-app.use(cors({
-    origin: "*"
-}))
+app.use(express.static("public"));
+app.use(
+	cors({
+		origin: "*",
+	})
+);
 
-const faceitData = require("./faceitData.json")
+const datapath = "/root/faceit_data/";
+
+const kills_path = datapath + "kills.json";
+const opening_kills = require(datapath + "opening_kills.json");
+const weapon_damages = require(datapath + "weapon_damages.json");
+const grenades = require(datapath + "grenades.json");
 
 app.get("/", (req, res) => {
-	res.send(faceitData)
-})
+	res.send({
+		status: "active",
+	});
+});
 
-app.get("/status", (req, res) => {
-    res.send({
-        "status" : "active"
-    })
-})
+app.get("/kills", (req, res) => {
+	const data = require(kills_path);
+    res.send(data)
+});
 
 server.listen(5000, () => {
-	console.log("Listening on port 5000 ...")
-})
+	console.log("Listening on port 5000 ...");
+});
