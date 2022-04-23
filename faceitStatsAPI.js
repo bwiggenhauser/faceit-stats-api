8ec07fc705cd61727c169ec1ec3d36f1d14fd60b
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2022-04-23 11:49:35 +0200

    added new last 30 matches endpoints

--- faceitStatsAPI.js
+++ faceitStatsAPI.js
@@ -12,32 +12,41 @@ app.use(
 );
 
 const datapath = "/root/faceit_data/";
-const newDatapath = "/root/new_faceit_data/";
+const newDatapathFull = "/root/new_faceit_data/full_data/";
+const newDatapathLast30 = "/root/new_faceit_data/last_30/";
 // const datapath = "C:/Users/baswi/Documents/GitHub/advanced-faceit-get-statistics/faceit_data/";
 
 const general_info_path = datapath + "general_information.json";
 const new_general_info_path = newDatapath + "general_information.json";
+const new_general_info_path_last_30 = newDatapath + "general_information.json";
 
 const kills_path = datapath + "kills.json";
-const new_kills_path = newDatapath + "kills.json";
+const new_kills_path = newDatapathFull + "kills.json";
+const new_kills_path_last30 = newDatapathLast30 + "kills.json";
 
 const opening_kills_path = datapath + "opening_kills.json";
-const new_opening_kills_path = newDatapath + "opening_kills.json";
+const new_opening_kills_path = newDatapathFull + "opening_kills.json";
+const new_opening_kills_path_last30 = newDatapathLast30 + "opening_kills.json";
 
 const weapon_damages_path = datapath + "weapon_damages.json";
-const new_weapon_damages_path = newDatapath + "weapon_damages.json";
+const new_weapon_damages_path = newDatapathFull + "weapon_damages.json";
+const new_weapon_damages_path_last30 = newDatapathLast30 + "weapon_damages.json";
 
 const grenades_path = datapath + "grenades.json";
-const new_grenades_path = newDatapath + "grenades.json";
+const new_grenades_path = newDatapathFull + "grenades.json";
+const new_grenades_path_last30 = newDatapathLast30 + "grenades.json";
 
 const damages_path = datapath + "damages.json";
-const new_damages_path = newDatapath + "damages.json";
+const new_damages_path = newDatapathFull + "damages.json";
+const new_damages_path_last30 = newDatapathLast30 + "damages.json";
 
 const deaths_path = datapath + "deaths.json";
-const new_deaths_path = newDatapath + "deaths.json";
+const new_deaths_path = newDatapathFull + "deaths.json";
+const new_deaths_path_last30 = newDatapathLast30 + "deaths.json";
 
 const trades_path = datapath + "trades.json";
-const new_trades_path = newDatapath + "trades.json";
+const new_trades_path = newDatapathFull + "trades.json";
+const new_trades_path_last30 = newDatapathLast30 + "trades.json";
 
 
 
@@ -48,6 +57,9 @@ app.get("/", (req, res) => {
 app.get("/new", (req, res) => {
 	res.sendFile(new_general_info_path);
 });
+app.get("/new_last_30", (req, res) => {
+	res.sendFile(new_general_info_path_last30);
+});
 
 
 
@@ -56,6 +68,9 @@ app.get("/kills", (req, res) => {
 });
 app.get("/new_kills", (req, res) => {
 	res.sendFile(new_kills_path);
+});
+app.get("/new_kills_last_30", (req, res) => {
+	res.sendFile(new_kills_path_last30);
 });
 
 
@@ -66,6 +81,9 @@ app.get("/deaths", (req, res) => {
 app.get("/new_deaths", (req, res) => {
 	res.sendFile(new_deaths_path);
 });
+app.get("/new_deaths_last_30", (req, res) => {
+	res.sendFile(new_deaths_path_last30);
+});
 
 
 
@@ -74,6 +92,9 @@ app.get("/openingkills", (req, res) => {
 });
 app.get("/new_openingkills", (req, res) => {
 	res.sendFile(new_opening_kills_path);
+});
+app.get("/new_openingkills_last_30", (req, res) => {
+	res.sendFile(new_opening_kills_path_last30);
 });
 
 
@@ -84,6 +105,9 @@ app.get("/damages", (req, res) => {
 app.get("/new_damages", (req, res) => {
 	res.sendFile(new_damages_path);
 });
+app.get("/new_damages_last_30", (req, res) => {
+	res.sendFile(new_damages_path_last30);
+});
 
 
 
@@ -92,6 +116,9 @@ app.get("/weapondamages", (req, res) => {
 });
 app.get("/new_weapondamages", (req, res) => {
 	res.sendFile(new_weapon_damages_path);
+});
+app.get("/new_weapondamages_last_30", (req, res) => {
+	res.sendFile(new_weapon_damages_path_last30);
 });
 
 
@@ -102,6 +129,9 @@ app.get("/trades", (req, res) => {
 app.get("/new_trades", (req, res) => {
 	res.sendFile(new_trades_path);
 });
+app.get("/new_trades_last_30", (req, res) => {
+	res.sendFile(new_trades_path_last30);
+});
 
 
 
@@ -110,6 +140,9 @@ app.get("/grenades", (req, res) => {
 });
 app.get("/new_grenades", (req, res) => {
 	res.sendFile(new_grenades_path);
+});
+app.get("/new_grenades_last_30", (req, res) => {
+	res.sendFile(new_grenades_path_last30);
 });
 
 server.listen(5000, () => {
